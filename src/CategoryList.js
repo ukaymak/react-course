6136051bd53b1252c1e9345ba38db64a4769ca80
ukaymak <unalkaymak@hotmail.com>
2020-07-09 14:10:43 +0300

    worked with state features

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
-import {ListGroup,ListGroupItem} from 'reactstrap'
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+ state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" }
+      ]
+    };
 
   render() {
     return (
@@ -9,11 +15,11 @@ export default class CategoryList extends Component {
         <h3>{this.props.info.title}</h3>
 
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+        {
+          this.state.categories.map(category => (
+          <ListGroupItem key={category.categoryId}> { category.categoryName}</ListGroupItem>
+          ))
+        }         
         </ListGroup>
       </div>
     );
