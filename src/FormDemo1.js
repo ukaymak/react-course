4adc898b2c1e12eda622d93a9be2da27c8e33549
Ünal KAYMAK <unal.kaymak@ibb.gov.tr>
2020-07-22 10:06:54 +0300

    multiple form example, name city

--- src/FormDemo1.js
+++ src/FormDemo1.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
 export default class FormDemo1 extends Component {
-  state = { userName: "" };
+  state = { userName: "" , city: "" };
 
   onChangeHandler = (event) => {
-    this.setState({ userName: event.target.value });
+   // this.setState({ userName: event.target.value });
+
+   let name     = event.target.name;
+   let value    = event.target.value; 
+
+   this.setState({[name]:value});
+
   };
 
   onSubmitHandler = (event) => {
@@ -17,8 +23,14 @@ export default class FormDemo1 extends Component {
       <div>
         <form onSubmit={this.onSubmitHandler}>
           <h3>User Name</h3>
-          <input onChange={this.onChangeHandler} type="text"></input>
+          <input name = "userName" onChange={this.onChangeHandler} type="text"></input>
           <h3>User Name is {this.state.userName}</h3>
+
+          <h3>City</h3>
+          <input name = "city" onChange={this.onChangeHandler} type="text"></input>
+          <h3>City is {this.state.city}</h3>
+
+
           <input type="submit" value="Save"></input>
         </form>
       </div>
